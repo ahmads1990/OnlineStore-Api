40312689e79ca59c383bbcf8ff0b50cf4b1c0395
Ahmad <ahmads20002000@gmail.com>
2024-08-11 03:09:29 +0300

    Configure ef added AppDbcontext

--- OnlineStore-Api/Program.cs
+++ OnlineStore-Api/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineStore_Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(connectionString, builder =>
+    {
+        builder.EnableRetryOnFailure(10, TimeSpan.FromSeconds(10), null);
+    });
+});
 
 var app = builder.Build();
 
---
+++ OnlineStore-Api/Services/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineStore_Api.Services;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
