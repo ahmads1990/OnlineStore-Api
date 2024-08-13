c33dfd444a0110ac9d2234cf58af00d1e68d8d1c
Ahmad Khaled <Ahmads20002000@gmail.com>
2024-08-14 01:37:32 +0300

    Add Product, ProductImage, Category models

---
+++ OnlineStore-Api/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineStore_Api.Models;
+
+public class Category
+{
+    public int CategoryID { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
---
+++ OnlineStore-Api/Models/ProductImage.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineStore_Api.Models;
+
+public class ProductImage
+{
+    public int ProductImageID { get; set; }
+    public byte Order { get; set; }
+    public string ImagePath { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
