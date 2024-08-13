a3af3f8129a6b9fa826ee51598fdc4e6b0644bee
Ahmad Khaled <Ahmads20002000@gmail.com>
2024-08-14 02:13:47 +0300

    Add IProductRepo and implementation

---
+++ OnlineStore-Api/Services/Interfaces/IProductRepo.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineStore_Api.Services.Interfaces;
+
+public interface IProductRepo
+{
+    Task<IEnumerable<Product>> GetAllProductsAsync(int? maxLimit);
+    //Task<IEnumerable<Product>> GetProductsWithPaginationAsync(string searchQuery, int pageNumber, int pageSize);
+    Task<Product?> GetFullProductByIDAsync(int productID);
+    Task<Product> AddNewProductAsync(Product product);
+    Task<Product> UpdateProductAsync(Product product);
+    Task<bool?> DeleteProductAsync(int productID);
+}
---
+++ OnlineStore-Api/Services/ProductRepo.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore_Api.Services.Interfaces;
+
+namespace OnlineStore_Api.Services;
+
+public class ProductRepo : IProductRepo
+{
+    private AppDbContext _context { get; set; }
+    public ProductRepo(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IEnumerable<Product>> GetAllProductsAsync(int? maxLimit)
+    {
+        var query = _context.Products
+                        .Include(p=>p.Category)
+                        .Include(p=>p.ProductImages)
+                        .AsQueryable();
+
+        if (maxLimit is not null && maxLimit > 0)
+            query = query.Take(maxLimit.Value);
+
+        return await query.ToListAsync();
+    }
+    public async Task<Product?> GetFullProductByIDAsync(int productID)
+    {
+        return await _context.Products
+                        .Include(p => p.Category)
+                        .Include(p => p.ProductImages)
+                        .FirstOrDefaultAsync(p => p.Id == productID);
+    }
+    public async Task<Product> AddNewProductAsync(Product product)
+    {
+        var newProduct =await _context.Products.AddAsync(product);
+        await _context.SaveChangesAsync();
+
+        return newProduct.Entity;
+    }
+    public async Task<Product> UpdateProductAsync(Product product)
+    {
+        var updatedProduct = _context.Products.Update(product);
+        await _context.SaveChangesAsync();
+
+        return updatedProduct.Entity;
+    }
+    public async Task<bool?> DeleteProductAsync(int productID)
+    {
+        var existingProduct = await GetFullProductByIDAsync(productID);
+        if (existingProduct is not null)
+        {
+            _context.Products.Remove(existingProduct);
+            var result =await _context.SaveChangesAsync();
+            return result > 0;
+        }
+        return null;
+    }
+}
