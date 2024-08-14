27762e65327de76ec4b30d5b283fb391cbbb4c78
Ahmad Khaled <Ahmads20002000@gmail.com>
2024-08-14 03:32:44 +0300

    Add ICategoryRepo and CategoryRepo

---
+++ OnlineStore-Api/Services/CategoryRepo.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineStore_Api.Services;
+
+public class CategoryRepo : ICategoryRepo
+{
+    private AppDbContext _context { get; set; }
+    public CategoryRepo(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+    {
+        return await _context.Categories.ToListAsync();
+    }
+    public async Task<Category?> GetCategoryWithIDAsync(int categoryID)
+    {
+        return await _context.Categories
+                        .FirstOrDefaultAsync(c => c.CategoryID == categoryID);
+    }
+    public async Task<Category?> GetCategoryWithNameAsync(string categoryTitle)
+    {
+        return await _context.Categories
+                      .FirstOrDefaultAsync(c => c.Title == categoryTitle);
+    }
+    public async Task<bool> CheckCategoryExistAsync(int categoryID)
+    {
+        return await _context.Categories
+                      .AnyAsync(c => c.CategoryID == categoryID);
+    }
+    public async Task<Category> AddNewCategoryAsync(Category category)
+    {
+        var newCategory = await _context.Categories.AddAsync(category);
+        await _context.SaveChangesAsync();
+
+        return newCategory.Entity;
+    }
+    public async Task<Category> UpdateCategoryAsync(Category category)
+    {
+        var updatedCategory = _context.Categories.Update(category);
+        await _context.SaveChangesAsync();
+
+        return updatedCategory.Entity;
+    }
+    public async Task<bool?> DeleteCategoryAsync(int categoryID)
+    {
+        var category = await GetCategoryWithIDAsync(categoryID);
+        if(category is not null)
+        {
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            var result = await _context.SaveChangesAsync();
+            return result > 0;
+        }
+        return null;
+    }
+}
---
+++ OnlineStore-Api/Services/Interfaces/ICategoryRepo.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineStore_Api.Services.Interfaces;
+
+public interface ICategoryRepo
+{
+    // Read
+    public Task<IEnumerable<Category>> GetAllCategoriesAsync();
+    public Task<Category?> GetCategoryWithIDAsync(int categoryID);
+    public Task<Category?> GetCategoryWithNameAsync(string categoryTitle);
+  
+    public Task<bool> CheckCategoryExistAsync(int categoryID);
+    // Create
+    public Task<Category> AddNewCategoryAsync(Category category);
+    // Update
+    public Task<Category> UpdateCategoryAsync(Category category);
+    // Delete
+    public Task<bool?> DeleteCategoryAsync(int categoryID);
+}
