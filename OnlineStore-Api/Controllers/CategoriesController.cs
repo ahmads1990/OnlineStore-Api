e69756ba21154a2c43579a175d78ece144558ff8
ahmads1990 <ahmads20002000@gmail.com>
2024-10-02 10:11:43 +0300

    Add CategoriesController

---
+++ OnlineStore-Api/Controllers/CategoriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore_Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CategoriesController : ControllerBase
+{
+    public CategoriesController()
+    {
+        
+    }
+}
