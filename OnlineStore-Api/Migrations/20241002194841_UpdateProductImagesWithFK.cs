71b6f782c975ebe0007a5d7587cc60b8b69d6f6b
Ahmad Khaled <Ahmads20002000@gmail.com>
2024-10-02 22:51:54 +0300

    Update ProductImage with fk and new migration

---
+++ OnlineStore-Api/Migrations/20241002194841_UpdateProductImagesWithFK.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineStore_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductImagesWithFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductImages_Products_ProductId",
+                table: "ProductImages");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "ProductImages",
+                newName: "ProductID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ProductImages_ProductId",
+                table: "ProductImages",
+                newName: "IX_ProductImages_ProductID");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductID",
+                table: "ProductImages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductImages_Products_ProductID",
+                table: "ProductImages",
+                column: "ProductID",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductImages_Products_ProductID",
+                table: "ProductImages");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductID",
+                table: "ProductImages",
+                newName: "ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ProductImages_ProductID",
+                table: "ProductImages",
+                newName: "IX_ProductImages_ProductId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "ProductImages",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductImages_Products_ProductId",
+                table: "ProductImages",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
--- OnlineStore-Api/Models/ProductImage.cs
+++ OnlineStore-Api/Models/ProductImage.cs
@@ -4,8 +4,9 @@ public class ProductImage
 {
     public int ProductImageID { get; set; }
     public byte Order { get; set; }
-    public string ImagePath { get; set; }
+    public string ImagePath { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
+
+    // Relations
     public int ProductID { get; set; }
-    public Product Product { get; set; }
 }
