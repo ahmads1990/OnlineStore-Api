3308aa163b708ea5b3078a013c94f718d824b947
ahmads1990 <ahmads20002000@gmail.com>
2024-10-02 09:44:29 +0300

    Add ProductsController

---
+++ OnlineStore-Api/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore_Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAllProducts(int? maxLimit)
+    {
+        var products = await _productService.GetAllProductsAsync(maxLimit);
+        return Ok(products);
+    }
+}
