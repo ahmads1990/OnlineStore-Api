2ed2e456c6b65057324c17e346e10eed1f762434
ahmads1990 <ahmads20002000@gmail.com>
2024-10-17 22:46:37 +0300

    Add model validation for dtos

--- OnlineStore-Api/Dtos/AddCategoryDto.cs
+++ OnlineStore-Api/Dtos/AddCategoryDto.cs
@@ -1,7 +1,11 @@
-﻿namespace OnlineStore_Api.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore_Api.Dtos;
 
 public class AddCategoryDto
 {
+    [Required, Length(5,50)]
     public string Title { get; set; } = string.Empty;
+    [Required, MaxLength(200)]
     public string Description { get; set; } = string.Empty;
 }
--- OnlineStore-Api/Dtos/AddProductDto.cs
+++ OnlineStore-Api/Dtos/AddProductDto.cs
@@ -1,13 +1,21 @@
-﻿namespace OnlineStore_Api.Dtos;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore_Api.Dtos;
 
 public class AddProductDto
 {
+    [Required, Length(5, 20)]
     public string Name { get; set; } = string.Empty;
+    [Required, Length(30, 200)]
     public string Description { get; set; } = string.Empty;
     public string Details { get; set; } = string.Empty;
+    [Required, Range(1, 1000)]
     public float Price { get; set; }
 
     // Relations
+    [Required, Range(1, int.MaxValue)]
     public int CategoryId { get; set; }
+    [ModelBinder(Name = "Images")]
     public IEnumerable<AddProductImageDto> ProductImageDtos { get; set; } = new List<AddProductImageDto>();
 }
