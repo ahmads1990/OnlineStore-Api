820b2d6a5a69ef54e3d2ceb65bdfd69f7d798563
ahmads1990 <ahmads20002000@gmail.com>
2024-10-18 00:27:47 +0300

    Update productDto JsonPropertyNames

--- OnlineStore-Api/Dtos/Product/ProductDto.cs
+++ OnlineStore-Api/Dtos/Product/ProductDto.cs
@@ -1,16 +1,22 @@
 ﻿using OnlineStore_Api.Dtos.Category;
 using OnlineStore_Api.Dtos.Product.ProductImage;
-using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace OnlineStore_Api.Dtos.Product;
 
 public class ProductDto
 {
+    [JsonPropertyName("Id")]
+    public int Id { get; set; }
+    [JsonPropertyName("Title")]
     public string Name { get; set; } = string.Empty;
+    [JsonPropertyName("Description")]
     public string Description { get; set; } = string.Empty;
+    [JsonPropertyName("Details")]
     public string Details { get; set; } = string.Empty;
+    [JsonPropertyName("Price")]
     public float Price { get; set; }
+    [JsonPropertyName("Category")]
     public CategoryDto Category { get; set; } = default!;
     [JsonPropertyName("Images")]
     public IEnumerable<ProductImageDto> ProductImages { get; set; } = new List<ProductImageDto>();
