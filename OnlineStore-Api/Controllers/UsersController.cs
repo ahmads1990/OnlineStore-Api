314d8b86a83dae380a7a03946a9a61baa37bfb2c
Ahmad Khaled <Ahmads20002000@gmail.com>
2024-11-10 13:51:58 +0200

    Add UsersController

---
+++ OnlineStore-Api/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore_Api.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var result = await _userService.GetAllUsers();
+        return Ok(result);
+
+    }
+    [HttpGet("{userId}")]
+    public async Task<IActionResult> GetUserById(string userId)
+    {
+        var result = await _userService.GetUserById(userId);
+
+        if (result is null)
+            return NotFound();
+
+        return Ok(result);
+    }
+}
