ed7198226334d21764f9fda3e914d436f9487964
Ahmad Khaled <Ahmads20002000@gmail.com>
2024-11-10 10:42:54 +0200

    Refactor move appDbContext class to models

--- OnlineStore-Api/Models/AppDbContext.cs
+++ OnlineStore-Api/Models/AppDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace OnlineStore_Api.Repositories;
+namespace OnlineStore_Api.Models;
 
 public class AppDbContext : IdentityDbContext<AppUser>
 {
